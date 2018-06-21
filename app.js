6dc4d44be21d40b033a6e47f27f988d8829a89c2
Geraldine <winand.geraldine@hotmail.com>
2018-06-21 16:22:55 +0200

    21-06-18 16h22

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
-/**********POP UP COOKIES**********/
+/**********POP UP AGE**********/
+$(document).ready(function(){
+	ageVerif();
+});
+
+function ageVerif() {
+
+	let ageVerif = prompt("Please enter your age !");
+	do{
+		if (ageVerif < 18) {
+			document.location.href="https://www.imdb.com/";
+		}else if (!Number(ageVerif) ) {
+			alert("pas cool man ! Ecris moi un nombre");
+			ageVerif = prompt("Please enter your age !");
+		}else if(ageVerif== null || ageVerif == "" ){
+			alert("Come on man! Ecris moi un nombre");
+			ageVerif = prompt("Please enter your age !");
+		}
+	}while(Number(ageVerif)>=18)
+}
 
 /**********POP UP CONTACT**********/
 function recup() {
