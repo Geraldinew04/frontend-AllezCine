738bdca15e9c9521132d583a26c79e2c67dfda7e
Geraldine <winand.geraldine@hotmail.com>
2018-06-29 11:53:01 +0200

    ca n'arrete pad de commiter ici

--- app.js
+++ app.js
@@ -53,3 +53,14 @@ $(document).ready(function(){
 	loginRegister();
 });
 
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/frontend-AllezCine/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
